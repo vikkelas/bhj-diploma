c6fc259ac4ce84ee39cfb6c69ac994933edf7955
Vikkelas <bass89@mail.ru>
2021-09-23 15:30:27 +0300

    2345

--- public/js/api/createRequest.js
+++ public/js/api/createRequest.js
@@ -2,30 +2,9 @@
  * Основная функция для совершения запросов
  * на сервер.
  * */
-const createRequest = (
-	options = {
-		url: 'https://example.com',
-		data: {
-			mail: 'ivan@biz.pro',
-			password: 'odinodin',
-		},
-		method: 'GET',
-		callback: (err, respose) => {
-			console.log(err)
-			console.log(respose)
-		},
-	}
-) => {
+const createRequest = (options = {}) => {
 	const xhr = new XMLHttpRequest()
-	let formData
 	xhr.open(options.method, options.url)
-	if (options.method !== 'GET') {
-		formData = new FormData()
-		formData.append('mail', options.data.mail)
-		formData.append('password', options.data.password)
-		xhr.send(formData)
-		xhr.responseType = 'json'
-	}
 	xhr.responseType = 'json'
 	xhr.send()
 	xhr.onreadystatechange = function () {
