c3058a2d2f790d37e3a18e3ebc07d2cfcf78a649
Vikkelas <bass89@mail.ru>
2021-09-29 01:13:30 +0300

    end create

--- public/js/api/createRequest.js
+++ public/js/api/createRequest.js
@@ -4,7 +4,30 @@
  * */
 const createRequest = (options = {}) => {
 	const xhr = new XMLHttpRequest()
+	const url = options.url
 	if (options.method === 'GET') {
+		url + options.data
 	}
-	xhr.open('url')
+	try {
+		xhr.open(options.method, url)
+		xhr.responseType = 'json'
+		if (options.method !== 'GET') {
+			const formData = new FormData()
+			for (let i in options.data) {
+				formData.append(i, options.data[i])
+			}
+			xhr.send(formData)
+		} else {
+			xhr.send(options.data)
+		}
+	} catch (e) {
+		options.callback(new Error(e.message))
+	}
+	xhr.addEventListener('loadend', () => {
+		if (xhr.response.success === true) {
+			options.callback(null, xhr.response)
+		} else {
+			options.callback(xhr.response.error)
+		}
+	})
 }
