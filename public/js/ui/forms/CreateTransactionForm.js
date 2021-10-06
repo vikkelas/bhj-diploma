819fe52d8ee28842b264156b58d6ffd58d54dbc4
vikkelas <bass89@mail.ru>
2021-10-06 23:33:00 +0300

    Modal window transaction

--- public/js/ui/forms/CreateTransactionForm.js
+++ public/js/ui/forms/CreateTransactionForm.js
@@ -17,8 +17,15 @@ class CreateTransactionForm extends AsyncForm {
 	 * Обновляет в форме всплывающего окна выпадающий список
 	 * */
 	renderAccountsList() {
-		const selectForm = this.element.querySelector('accounts-select')
-		Account.list()
+		const user = JSON.parse(localStorage.user)
+		const selectForm = this.element.querySelector('.accounts-select')
+		let index = ''
+		Account.list(user, (err, response) => {
+			response.data.forEach(element => {
+				index += `<option value="${element.id}">${element.name}</option>`
+			})
+			selectForm.innerHTML = index
+		})
 	}
 
 	/**
@@ -27,5 +34,17 @@ class CreateTransactionForm extends AsyncForm {
 	 * вызывает App.update(), сбрасывает форму и закрывает окно,
 	 * в котором находится форма
 	 * */
-	onSubmit(data) {}
+	onSubmit(data) {
+		let modal
+		this.element.getAttribute('id') === 'new-income-form'
+			? (modal = 'newIncome')
+			: (modal = 'newExpense')
+		Transaction.create(data, (err, response) => {
+			if (response.success) {
+				this.element.reset()
+				App.modals[modal].close()
+				App.update()
+			}
+		})
+	}
 }
